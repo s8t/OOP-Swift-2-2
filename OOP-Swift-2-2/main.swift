e3f4ee7b74f0e7d473669af814b8a8b655afad6d
Anton Stepanov <to@stepa.dev>
2021-01-13 17:41:04 +0300

    There is some unsafe prodedural code

--- OOP-Swift-2-2/main.swift
+++ OOP-Swift-2-2/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
+//let rows: [String] = [
+//    "Ivan;Petrov;01.01.01",
+//    "Petr;Ivanov;02.02.02"
+//]
 
+let file = Bundle.main.path(forResource: "list", ofType: "txt")
+let contents = try String(contentsOfFile: file!)
+let rows = contents.split(separator:"\n")
+
+for row in rows {
+    let part = row.split(separator: ";")
+    print("\(part[0]) \(part[1]) \(part[2])")
+}
 
