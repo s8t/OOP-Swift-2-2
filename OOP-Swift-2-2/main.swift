503ba8b4fa83a6d32875a6dc6110074913ab3827
Anton Stepanov <to@stepa.dev>
2021-01-14 00:35:09 +0300

    Refactoring loadStudentsFromTxt method and added saveStudentsToTxt procedure

--- OOP-Swift-2-2/main.swift
+++ OOP-Swift-2-2/main.swift
@@ -26,17 +26,32 @@ struct Student {
 }
 
 func loadStudentsFromTxt(_ file: String) -> [Int:Student] {
-    
-    let contents = try! String(contentsOfFile: file)
-    let rows = contents.split(separator:"\n")
+       
     var students = [Int:Student]()
-    
-    for (index, row) in rows.enumerated() {
-        let part = row.split(separator: ";")
-        let student = Student(String(part[0]), String(part[1]), String(part[2]))
-        students[index] = student
+    do {
+        let contents = try String(contentsOfFile: file)
+        let rows = contents.split(separator:"\n")
+        for (index, row) in rows.enumerated() {
+            let part = row.split(separator: ";")
+            let student = Student(String(part[0]), String(part[1]), String(part[2]))
+            students[index] = student
+        }
+    } catch {
+        // ... something went wrong
     }
+    
     return students
+}
+
+
+func saveStudentsToTxt(_ students: [Int:Student], _ file: String) {
+
+    var rows = [String]()
+    for student in students.values {
+        let row = "\(student.firstName);\(student.lastName);\(student.birthDate)"
+        rows.append(row)
+    }
+    // ... write to file
 }
 
 /// ---Setup App---------------------------------------------------
@@ -50,3 +65,7 @@ let students = loadStudentsFromTxt(file)
 for student in students.values {
     print("\(student.fullName) \(student.birthDate)")
 }
+
+saveStudentsToTxt(students, file)
+
+
