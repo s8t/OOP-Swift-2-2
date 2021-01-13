db4b322db6c7af43a7f4f88b56e9ef644d83c261
Anton Stepanov <to@stepa.dev>
2021-01-13 18:32:13 +0300

    Moved logic for prepare students array into a function

--- OOP-Swift-2-2/main.swift
+++ OOP-Swift-2-2/main.swift
@@ -7,20 +7,30 @@
 
 import Foundation
 
-let file = Bundle.main.path(forResource: "list", ofType: "txt")
-let contents = try String(contentsOfFile: file!)
-let rows = contents.split(separator:"\n")
-
-var students = [Int:[String:String]]()
-
-for (index, row) in rows.enumerated() {
-    let part = row.split(separator: ";")
-    students[index] = [
-        "firstName" : String(part[0]),
-        "lastName" : String(part[1]),
-        "birthDate" : String(part[2])
-    ]
+func loadStudentsFromTxt(_ file: String) -> [Int:[String:String]] {
+    
+    let contents = try! String(contentsOfFile: file)
+    let rows = contents.split(separator:"\n")
+    var students = [Int:[String:String]]()
+    
+    for (index, row) in rows.enumerated() {
+        let part = row.split(separator: ";")
+        students[index] = [
+            "firstName" : String(part[0]),
+            "lastName" : String(part[1]),
+            "birthDate" : String(part[2])
+        ]
+    }
+    return students
 }
+
+/// ---Setup App---------------------------------------------------
+
+let file = Bundle.main.path(forResource: "list", ofType: "txt")!
+
+/// ---Main code---------------------------------------------------
+
+let students = loadStudentsFromTxt(file)
 
 for student in students.values {
     print("\(String(student["firstName"]!)) \(String(student["lastName"]!)) \(String(student["birthDate"]!))")
